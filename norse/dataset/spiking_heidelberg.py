9e952400a141bae958f2d5ac10b5ee5358c7fb8b
Jens Egholm <jensegholm@protonmail.com>
2020-12-31 12:48:24 +0100

    Moved Spiking Heidelberg data to iterable

--- norse/dataset/spiking_heidelberg.py
+++ norse/dataset/spiking_heidelberg.py
@@ -8,15 +8,15 @@ The Heidelberg spiking datasets for the systematic evaluation of spiking neural 
 ArXiv:1910.07407 [Cs, q-Bio]. https://arxiv.org/abs/1910.07407
 """
 
+from itertools import chain
 import os
 import h5py
-import tqdm
 
 import torch
 from torchvision.datasets.utils import check_integrity, download_and_extract_archive
 
 
-class SpikingHeidelberg(torch.utils.data.Dataset):
+class SpikingHeidelberg(torch.utils.data.IterableDataset):
     """
     Initialises, but does not download by default, the
     `Spiking Heidelberg audio dataset <https://compneuro.net/posts/2019-spiking-heidelberg-digits/>`_.
@@ -37,6 +37,7 @@ class SpikingHeidelberg(torch.utils.data.Dataset):
     train_file, train_file_md5 = "shd_train.h5", "8e9877c85f29a28dd353ea6a54402667"
     train_url = "https://compneuro.net/datasets/shd_train.h5.gz"
     train_checksum = "d47c9825dee33347913e8ce0f2be08b0"
+    max_length = 1.4  # Max dataset length in seconds
     n_units = 700  # 700 channels
 
     def __init__(self, root, dt=0.001, sparse=True, train=True, download=False):
@@ -45,44 +46,39 @@ class SpikingHeidelberg(torch.utils.data.Dataset):
         self.train = train
         self.dt = dt
 
-        self.data_dir = os.path.join(self.root, "shd_data")
-        if not os.path.isdir(self.data_dir):
-            os.mkdir(self.data_dir)
-        self.test_data_file = os.path.join(self.data_dir, "test.dat")
-        self.train_data_file = os.path.join(self.data_dir, "train.dat")
-
         if download:
             self.download()
 
-        self.data = []
-        if train:
-            self.data = self.data + torch.load(self.train_data_file)
-        self.data = self.data + torch.load(self.test_data_file)
+        self.train_fp = (
+            None
+            if not self.train
+            else h5py.File(os.path.join(self.root, self.train_file), "r")
+        )
+        self.test_fp = h5py.File(os.path.join(self.root, self.test_file), "r")
 
     def _check_integrity(self):
-        return os.path.isfile(self.train_data_file) and os.path.isfile(
-            self.test_data_file
+        return check_integrity(
+            os.path.join(self.root, self.test_file), self.test_file_md5
+        ) and (
+            not self.train
+            or check_integrity(
+                os.path.join(self.root, self.train_file), self.train_file_md5
+            )
         )
 
-    def _generate_data(self, h5file):
-        with h5py.File(h5file, "r") as fp:
-            points = []
-            for times, units, label in tqdm.tqdm(
-                zip(fp["spikes"]["times"], fp["spikes"]["units"], fp["labels"]),
-                total=len(fp["labels"]),
-            ):
-                points.append((self._bin_spikes(times, units), label))
-            return points
+    def _data_iterator(self, fp):
+        return zip(fp["spikes"]["times"], fp["spikes"]["units"], fp["labels"])
 
-    def _bin_spikes(self, times, units):
+    def _bin_spikes(self, tuple):
+        times, units, label = tuple
         assert len(times) == len(units), "Spikes and units must have same length"
-        length = int(times.max() // self.dt + 1)
+        length = int(self.max_length // self.dt + 1)
         data = torch.zeros((length, self.n_units), dtype=torch.uint8)
         for index in range(len(times)):
             time_index = int(times[index] // self.dt)
             unit_index = units[index]
             data[time_index][unit_index] = 1
-        return data.to_sparse()
+        return data.to_sparse(), torch.as_tensor(int(label), dtype=torch.uint8)
 
     def download(self):
         if not self._check_integrity():
@@ -90,16 +86,14 @@ class SpikingHeidelberg(torch.utils.data.Dataset):
                 download_and_extract_archive(
                     self.train_url, self.root, md5=self.train_checksum
                 )
-                data_train = self._generate_data(self.train_file)
-                torch.save(data_train, self.train_data_file)
             download_and_extract_archive(
                 self.test_url, self.root, md5=self.test_checksum
             )
-            data_test = self._generate_data(self.test_file)
-            torch.save(data_test, self.test_data_file)
 
-    def __getitem__(self, index):
-        return self.data[index]
-
-    def __len__(self):
-        return len(self.data)
+    def __iter__(self):
+        iterator = map(self._bin_spikes, self._data_iterator(self.test_fp))
+        if self.train:
+            iterator = chain(
+                map(self._bin_spikes, self._data_iterator(self.test_fp)), iterator
+            )
+        return iterator
